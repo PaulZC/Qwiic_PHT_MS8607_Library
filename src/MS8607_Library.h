e8f08a06b7b46881c6d94bdab44ba115544a906f
Nathan Seidle <nathan@sparkfun.com>
2020-04-10 10:04:20 -0600

    VScode whitespace changes
    We use VScode to autoformat all our code. This is only whitespace changes.

--- src/MS8607_Library.h
+++ src/MS8607_Library.h
@@ -48,62 +48,79 @@
 #include "WProgram.h"
 #endif
 
-enum MS8607_humidity_i2c_master_mode { MS8607_i2c_hold, MS8607_i2c_no_hold };
-
-enum MS8607_status {
-  MS8607_status_ok,
-  MS8607_status_no_i2c_acknowledge,
-  MS8607_status_i2c_transfer_error,
-  MS8607_status_crc_error,
-  MS8607_status_heater_on_error
+enum MS8607_humidity_i2c_master_mode
+{
+       MS8607_i2c_hold,
+       MS8607_i2c_no_hold
 };
 
-enum MS8607_humidity_resolution {
-  MS8607_humidity_resolution_12b = 0,
-  MS8607_humidity_resolution_8b,
-  MS8607_humidity_resolution_10b,
-  MS8607_humidity_resolution_11b
+enum MS8607_status
+{
+       MS8607_status_ok,
+       MS8607_status_no_i2c_acknowledge,
+       MS8607_status_i2c_transfer_error,
+       MS8607_status_crc_error,
+       MS8607_status_heater_on_error
 };
 
-enum MS8607_battery_status { MS8607_battery_ok, MS8607_battery_low };
-
-enum MS8607_heater_status { MS8607_heater_off, MS8607_heater_on };
-
-enum MS8607_pressure_resolution {
-  MS8607_pressure_resolution_osr_256 = 0,
-  MS8607_pressure_resolution_osr_512,
-  MS8607_pressure_resolution_osr_1024,
-  MS8607_pressure_resolution_osr_2048,
-  MS8607_pressure_resolution_osr_4096,
-  MS8607_pressure_resolution_osr_8192
+enum MS8607_humidity_resolution
+{
+       MS8607_humidity_resolution_12b = 0,
+       MS8607_humidity_resolution_8b,
+       MS8607_humidity_resolution_10b,
+       MS8607_humidity_resolution_11b
 };
 
-enum i2c_status_code {
-  i2c_status_ok = 0x00,
-  i2c_status_err_overflow = 0x01,
-  i2c_status_err_timeout = 0x02,
+enum MS8607_battery_status
+{
+       MS8607_battery_ok,
+       MS8607_battery_low
 };
 
-class MS8607 {
+enum MS8607_heater_status
+{
+       MS8607_heater_off,
+       MS8607_heater_on
+};
+
+enum MS8607_pressure_resolution
+{
+       MS8607_pressure_resolution_osr_256 = 0,
+       MS8607_pressure_resolution_osr_512,
+       MS8607_pressure_resolution_osr_1024,
+       MS8607_pressure_resolution_osr_2048,
+       MS8607_pressure_resolution_osr_4096,
+       MS8607_pressure_resolution_osr_8192
+};
+
+enum i2c_status_code
+{
+       i2c_status_ok = 0x00,
+       i2c_status_err_overflow = 0x01,
+       i2c_status_err_timeout = 0x02,
+};
+
+class MS8607
+{
 
 public:
-  MS8607();
+       MS8607();
 
-  /*
+       /*
    \brief Perform initial configuration. Has to be called once.
   */
-  bool begin(TwoWire &wirePort = Wire);
+       bool begin(TwoWire &wirePort = Wire);
 
-  /*
+       /*
   \brief Check whether MS8607 device is connected
   
   \return bool : status of MS8607
          - true : Device is present
          - false : Device is not acknowledging I2C address
   */
-  bool isConnected(void);
+       bool isConnected(void);
 
-  /*
+       /*
    \brief Reset the MS8607 device
    
    \return MS8607_status : status of MS8607
@@ -111,9 +128,9 @@ public:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status reset(void);
+       enum MS8607_status reset(void);
 
-  /*
+       /*
    \brief Enable heater
    
    \return MS8607_status : status of MS8607
@@ -121,9 +138,9 @@ public:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status enable_heater(void);
+       enum MS8607_status enable_heater(void);
 
-  /*
+       /*
    \brief Disable heater
    
    \return MS8607_status : status of MS8607
@@ -131,9 +148,9 @@ public:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status disable_heater(void);
+       enum MS8607_status disable_heater(void);
 
-  /*
+       /*
    \brief Get heater status
    
    \param[in] MS8607_heater_status* : Return heater status (above or below 2.5V)
@@ -145,9 +162,9 @@ public:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status get_heater_status(enum MS8607_heater_status *heater);
+       enum MS8607_status get_heater_status(enum MS8607_heater_status *heater);
 
-  /*
+       /*
    \brief Provide battery status
    
    \param[out] MS8607_battery_status* : Battery status
@@ -159,9 +176,9 @@ public:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status get_battery_status(enum MS8607_battery_status *bat);
+       enum MS8607_status get_battery_status(enum MS8607_battery_status *bat);
 
-  /*
+       /*
    \brief Reads the temperature, pressure and relative humidity value.
    
    \param[out] float* : degC temperature value
@@ -174,12 +191,12 @@ public:
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
           - MS8607_status_crc_error : CRC check error
   */
-  enum MS8607_status read_temperature_pressure_humidity(float *t, float *p,
-                                                        float *h);
+       enum MS8607_status read_temperature_pressure_humidity(float *t, float *p,
+                                                             float *h);
 
-  /******************** Functions from humidity sensor ********************/
+       /******************** Functions from humidity sensor ********************/
 
-  /*
+       /*
    \brief Set humidity ADC resolution.
    
    \param[in] MS8607_humidity_resolution : Resolution requested
@@ -190,10 +207,10 @@ public:
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
           - MS8607_status_crc_error : CRC check error
   */
-  enum MS8607_status
-  set_humidity_resolution(enum MS8607_humidity_resolution res);
+       enum MS8607_status
+       set_humidity_resolution(enum MS8607_humidity_resolution res);
 
-  /*
+       /*
    \brief Set Humidity sensor ADC resolution.
    
    \param[in] MS8607_i2c_master_mode : I2C mode
@@ -201,9 +218,9 @@ public:
    \return MS8607_status : status of MS8607
           - MS8607_status_ok
   */
-  void set_humidity_i2c_master_mode(enum MS8607_humidity_i2c_master_mode mode);
+       void set_humidity_i2c_master_mode(enum MS8607_humidity_i2c_master_mode mode);
 
-  /*
+       /*
    \brief Returns result of compensated humidity
           Note : This function shall only be used when the heater is OFF. It
           will return an error otherwise.
@@ -217,11 +234,11 @@ public:
           - MS8607_status_heater_on_error : Cannot compute compensated humidity
             because heater is on
   */
-  enum MS8607_status get_compensated_humidity(float temperature,
-                                              float relative_humidity,
-                                              float *compensated_humidity);
+       enum MS8607_status get_compensated_humidity(float temperature,
+                                                   float relative_humidity,
+                                                   float *compensated_humidity);
 
-  /*
+       /*
    \brief Returns the computed dew point
            Note : This function shall only be used when the heater is OFF. It
            will return an error otherwise.
@@ -235,38 +252,37 @@ public:
           - MS8607_status_heater_on_error : Cannot compute compensated humidity
             because heater is on
   */
-  enum MS8607_status get_dew_point(float temperature, float relative_humidity,
-                                   float *dew_point);
+       enum MS8607_status get_dew_point(float temperature, float relative_humidity,
+                                        float *dew_point);
 
-  /******************** Functions from Pressure sensor ********************/
-  /*
+       /******************** Functions from Pressure sensor ********************/
+       /*
    \brief Set pressure ADC resolution.
    
    \param[in] MS8607_pressure_resolution : Resolution requested
    
   */
-  void set_pressure_resolution(enum MS8607_pressure_resolution res);
+       void set_pressure_resolution(enum MS8607_pressure_resolution res);
 
-  float getPressure(); //Returns the latest pressure measurement
-  float getTemperature(); //Returns the latest temperature measurement
-  float getHumidity(); //Returns the latest humidity measurement
-  double adjustToSeaLevel(double absolutePressure, double actualAltitude);
-  double altitudeChange(double currentPressure, double baselinePressure);
-
+       float getPressure();    //Returns the latest pressure measurement
+       float getTemperature(); //Returns the latest temperature measurement
+       float getHumidity();    //Returns the latest humidity measurement
+       double adjustToSeaLevel(double absolutePressure, double actualAltitude);
+       double altitudeChange(double currentPressure, double baselinePressure);
 
 private:
-  /******************** Functions from humidity sensor ********************/
+       /******************** Functions from humidity sensor ********************/
 
-  /*
+       /*
    \brief Check whether humidity sensor is connected
    
    \return bool : status of humidity sensor
           - true : Device is present
           - false : Device is not acknowledging I2C address
   */
-  bool hsensor_is_connected(void);
+       bool hsensor_is_connected(void);
 
-  /*
+       /*
    \brief Reset the humidity sensor part
    
    \return MS8607_status : status of MS8607
@@ -274,9 +290,9 @@ private:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status hsensor_reset(void);
+       enum MS8607_status hsensor_reset(void);
 
-  /*
+       /*
    \brief Check CRC
    
    \param[in] uint16_t : variable on which to check CRC
@@ -286,9 +302,9 @@ private:
           - MS8607_status_ok : CRC check is OK
           - MS8607_status_crc_error : CRC check error
   */
-  enum MS8607_status hsensor_crc_check(uint16_t value, uint8_t crc);
+       enum MS8607_status hsensor_crc_check(uint16_t value, uint8_t crc);
 
-  /*
+       /*
    \brief Reads the MS8607 humidity user register.
    
    \param[out] uint8_t* : Storage of user register value
@@ -298,9 +314,9 @@ private:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status hsensor_read_user_register(uint8_t *value);
+       enum MS8607_status hsensor_read_user_register(uint8_t *value);
 
-  /*
+       /*
    \brief Writes the MS8607 humidity user register with value
            Will read and keep the unreserved bits of the register
    
@@ -311,9 +327,9 @@ private:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status hsensor_write_user_register(uint8_t value);
+       enum MS8607_status hsensor_write_user_register(uint8_t value);
 
-  /*
+       /*
    \brief Set Humidity sensor ADC resolution.
    
    \param[in] MS8607_i2c_master_mode : I2C mode
@@ -321,9 +337,9 @@ private:
    \return MS8607_status : status of MS8607
           - MS8607_status_ok
   */
-  enum MS8607_humidity_i2c_master_mode hsensor_i2c_master_mode;
+       enum MS8607_humidity_i2c_master_mode hsensor_i2c_master_mode;
 
-  /*
+       /*
    \brief Reads the relative humidity ADC value
    
    \param[out] uint16_t* : Relative humidity ADC value.
@@ -334,9 +350,9 @@ private:
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
           - MS8607_status_crc_error : CRC check error
   */
-  enum MS8607_status hsensor_humidity_conversion_and_read_adc(uint16_t *adc);
+       enum MS8607_status hsensor_humidity_conversion_and_read_adc(uint16_t *adc);
 
-  /*
+       /*
    \brief Reads the relative humidity value.
    
    \param[out] float* : %RH Relative Humidity value
@@ -347,20 +363,20 @@ private:
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
           - MS8607_status_crc_error : CRC check error
   */
-  enum MS8607_status hsensor_read_relative_humidity(float *humidity);
+       enum MS8607_status hsensor_read_relative_humidity(float *humidity);
 
-  /******************** Functions from Pressure sensor ********************/
+       /******************** Functions from Pressure sensor ********************/
 
-  /*
+       /*
    \brief Check whether MS8607 pressure sensor is connected
    
    \return bool : status of MS8607
           - true : Device is present
           - false : Device is not acknowledging I2C address
   */
-  bool psensor_is_connected(void);
+       bool psensor_is_connected(void);
 
-  /*
+       /*
    \brief Reset the Pressure sensor part
    
    \return MS8607_status : status of MS8607
@@ -368,9 +384,9 @@ private:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status psensor_reset(void);
+       enum MS8607_status psensor_reset(void);
 
-  /*
+       /*
    \brief Reads the psensor EEPROM coefficient stored at address provided.
    
    \param[in] uint8_t : Address of coefficient in EEPROM
@@ -382,10 +398,10 @@ private:
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
           - MS8607_status_crc_error : CRC check error on the coefficients
   */
-  enum MS8607_status psensor_read_eeprom_coeff(uint8_t command,
-                                               uint16_t *coeff);
+       enum MS8607_status psensor_read_eeprom_coeff(uint8_t command,
+                                                    uint16_t *coeff);
 
-  /*
+       /*
    \brief Reads the MS8607 EEPROM coefficients to store them for computation.
    
    \return MS8607_status : status of MS8607
@@ -394,9 +410,9 @@ private:
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
           - MS8607_status_crc_error : CRC check error on the coefficients
   */
-  enum MS8607_status psensor_read_eeprom(void);
+       enum MS8607_status psensor_read_eeprom(void);
 
-  /*
+       /*
    \brief CRC check
    
    \param[in] uint16_t *: List of EEPROM coefficients
@@ -404,9 +420,9 @@ private:
    
    \return bool : TRUE if CRC is OK, FALSE if KO
   */
-  bool psensor_crc_check(uint16_t *n_prom, uint8_t crc);
+       bool psensor_crc_check(uint16_t *n_prom, uint8_t crc);
 
-  /*
+       /*
    \brief Compute temperature and pressure
    
    \param[out] float* : Celsius Degree temperature value
@@ -418,10 +434,10 @@ private:
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
           - MS8607_status_crc_error : CRC check error on the coefficients
   */
-  enum MS8607_status psensor_read_pressure_and_temperature(float *temperature,
-                                                           float *pressure);
+       enum MS8607_status psensor_read_pressure_and_temperature(float *temperature,
+                                                                float *pressure);
 
-  /*
+       /*
    \brief Triggers conversion and read ADC value
    
    \param[in] uint8_t : Command used for conversion (will determine
@@ -433,19 +449,19 @@ private:
           - MS8607_status_i2c_transfer_error : Problem with i2c transfer
           - MS8607_status_no_i2c_acknowledge : I2C did not acknowledge
   */
-  enum MS8607_status psensor_conversion_and_read_adc(uint8_t cmd,
-                                                     uint32_t *adc);
+       enum MS8607_status psensor_conversion_and_read_adc(uint8_t cmd,
+                                                          uint32_t *adc);
 
-  uint32_t hsensor_conversion_time;
-  bool hsensor_heater_on;
-  uint32_t psensor_conversion_time[6];
+       uint32_t hsensor_conversion_time;
+       bool hsensor_heater_on;
+       uint32_t psensor_conversion_time[6];
 
-  TwoWire *_i2cPort; //The generic connection to user's chosen I2C hardware
-  float globalPressure;
-  boolean pressureHasBeenRead = true;
-  float globalTemperature;
-  boolean temperatureHasBeenRead = true;
-  float globalHumidity;
-  boolean humidityHasBeenRead = true;
+       TwoWire *_i2cPort; //The generic connection to user's chosen I2C hardware
+       float globalPressure;
+       boolean pressureHasBeenRead = true;
+       float globalTemperature;
+       boolean temperatureHasBeenRead = true;
+       float globalHumidity;
+       boolean humidityHasBeenRead = true;
 };
 #endif
